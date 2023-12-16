a662ee49dbb4eb99c617f91cb4fc54279d0b16c5
martin.calsina <martincalsina05@gmail.com>
2023-12-16 16:18:15 -0300

    validacion con formularios reactivos en el registro

--- src/app/signup/components/signup/signup.component.ts
+++ src/app/signup/components/signup/signup.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { DataService } from '../../../services/data.service';
 import { Persona } from '../../../shared/models/shared.models.persona';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
-export class SignupComponent implements OnInit, OnChanges{
+export class SignupComponent implements OnInit {
+
+  signupForm: FormGroup | undefined;
 
   public nombre: string = "";
   public apellido: string = "";
@@ -15,29 +18,53 @@ export class SignupComponent implements OnInit, OnChanges{
   public password: string = "";
   public confirmedPassword: string = "";
 
-  constructor(private dataService: DataService) {
+  constructor(private fb: FormBuilder, private dataService: DataService) {
 
   }
 
   ngOnInit(): void {
-    
+    this.signupForm = this.fb.group({
+      nombre: ['', [Validators.required, Validators.maxLength(100)]],
+      apellido: ['', [Validators.required, Validators.maxLength(100)]],
+      correo: ['', [Validators.required, Validators.email, Validators.maxLength(150)]],
+      password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(100)]],
+      confirmPassword: ['', [Validators.required, this.matchPassword.bind(this)]],
+    });
   }
 
-  ngOnChanges(): void {
-    
+  matchPassword(control: FormControl) {
+    const password = this.signupForm ? this.signupForm.get('password')!.value : '';
+    return password === control.value ? null : { mismatch: true };
+  }
+
+  inputValido(formControlName: string): boolean {
+    if (this.signupForm!.get(formControlName)?.valid) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   registrarUsuario(): void {
 
-    this.dataService.correoDisponible(this.correo.toLowerCase()).subscribe((correoValido) => {
-      if (correoValido) {
-        let persona: Persona = new Persona(this.nombre, this.apellido, this.correo, this.password);
-        this.dataService.registrarUsuario(persona);
-        console.log("Usuario creado");
-      } else {
-        console.log("El correo ya se encuentra registrado");
-      }
-    });
+    if (this.signupForm!.valid) {
+
+      console.log('Formulario válido', this.signupForm!.value);
+
+      this.dataService.correoDisponible(this.correo.toLowerCase()).subscribe((correoValido) => {
+        if (correoValido) {
+          let persona: Persona = new Persona(this.nombre, this.apellido, this.correo, this.password);
+          this.dataService.registrarUsuario(persona);
+          console.log("Usuario creado");
+        } else {
+          console.log("El correo ya se encuentra registrado");
+        }
+      });
+
+    } else {
+      // Mostrar errores al usuario
+      console.log('Formulario inválido. Corrija los errores.');
+    }
 
   }
 
--- src/app/signup/signup.module.ts
+++ src/app/signup/signup.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { SignupRoutingModule } from './signup-routing.module';
 import { SignupComponent } from './components/signup/signup.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -13,7 +13,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     SignupRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports: [
   ]
