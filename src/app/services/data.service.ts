270d67235a3c8b3c79059f55205c32cb0d5509ef
martin.calsina <martincalsina05@gmail.com>
2023-12-17 16:32:43 -0300

    cambiado el backend al deploy de la nube

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -9,7 +9,8 @@ import { Entrenamiento } from '../shared/models/shared.models.entrenamiento';
 })
 export class DataService {
 
-  backendUrl = "http://localhost:8080";
+  //backendUrl = "http://localhost:8080"; //local
+  backendUrl = "https://backend-gimnasio.onrender.com"; //produccion
 
   private rutinasSubject = new Subject<void>();
   private entrenamientosSubject = new Subject<void>();
